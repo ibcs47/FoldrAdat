8913c7962e9bcb3d1f9a4460477763a650665874
ibcs <ifj.benko.csaba997@gmail.com>
2020-09-03 11:02:32 +0200

    Otthoni munka feltoltese: -Kategoriavalaszto -Lista- es grid nezet -URL-en keresztuli navigacio

---
+++ src/app/mock.ts
@@ -0,0 +1,28 @@
+import { Varos, Orszag, Kontinens} from './kategoria';
+
+export const VAROS: Varos[]=[
+  {id: 1120985,name: "Zaranj"},
+  {id: 1123004,name: "Taloqan"},
+  {id: 1125896,name: "Shahrak"},
+  {id: 1127110,name: "Sar-e Pul"},
+  {id: 1133453,name: "Maymana"},
+  {id: 174875,name: "Kapan"},
+  {id: 174895,name: "Goris"},
+  {id: 714581,name: "Törökszentmiklós"},
+  {id: 715126,name: "Szolnok"},
+  {id: 715429,name: "Szeged"}
+];
+export const ORSZAG: Orszag[]=[
+  {id: 1149361,name: "Afghanistan"},
+  {id: 174982,name: "Armenia"},
+  {id: 719819,name: "Hungary"}
+];
+export const KONTINENS: Kontinens[]=[
+  {id: 1,name: "Africa"},
+  {id: 2,name: "Antarctica"},
+  {id: 3,name: "Asia"},
+  {id: 4,name: "Europe"},
+  {id: 5,name: "North America"},
+  {id: 6,name: "Oceania"},
+  {id: 7,name: "South America"}
+];
